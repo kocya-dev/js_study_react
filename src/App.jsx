22a4a4ff7437946b4551649ad64d7662040c4552
kocya <f2_hanabi@yahoo.co.jp>
2023-01-03 04:29:22 +0000

    React: stateサンプル追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const onClickButton = () => {
-    alert();
+    setNum(num + 1);
   };
+  const [num, setNum] = useState(0); // 変数名=num(初期値=0),  setter = setNum
 
   return (
     <>
       <ColorfulMessage color="Red">hello</ColorfulMessage>
       <ColorfulMessage color="blue">hello2</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickButton}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
