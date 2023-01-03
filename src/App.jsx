3d64e7db57a42b7a4452f0f4730ceda620ae464a
kocya <f2_hanabi@yahoo.co.jp>
2023-01-03 04:34:21 +0000

    React: ステート変化に応じた再レンダリングのサンプル

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ const App = () => {
   const onClickButton = () => {
     setNum(num + 1);
   };
+  const onClickSwitchShowFlag = () => {
+    // ボタン押下でステート変更 -> ステート変化を受けて再レンダリングされる
+    setFaceShowFlag(!faceShowFlag);
+  };
   const [num, setNum] = useState(0); // 変数名=num(初期値=0),  setter = setNum
+  const [faceShowFlag, setFaceShowFlag] = useState(false); // 変数名=num(初期値=0),  setter = setNum
 
   return (
     <>
@@ -13,6 +18,9 @@ const App = () => {
       <ColorfulMessage color="blue">hello2</ColorfulMessage>
       <button onClick={onClickButton}>カウントアップ</button>
       <p>{num}</p>
+
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      {faceShowFlag && <p>face </p>}
     </>
   );
 };
