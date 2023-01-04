50a890c84f619b2b6afe9b7f937f1d05fc58a2b6
kocya <f2_hanabi@yahoo.co.jp>
2023-01-04 03:39:49 +0000

    ReactでTODOアプリ

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import "./styles.css";
 import React, { useState, useEffect } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  /*
   const [num, setNum] = useState(0); // 変数名=num(初期値=0),  setter = setNum
   const [faceShowFlag, setFaceShowFlag] = useState(false); // 変数名=num(初期値=0),  setter = setNum
   useEffect(() => {
@@ -23,16 +25,101 @@ const App = () => {
     // ボタン押下でステート変更 -> ステート変化を受けて再レンダリングされる
     setFaceShowFlag(!faceShowFlag);
   };
+  */
+
+  const [inputTodoName, setInputTodoName] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+  const onTextChange = (event) => setInputTodoName(event.target.value);
+  const onClickAdd = () => {
+    if (inputTodoName === "") return; // 空文字では何もしない
+    // 既存の未完了TODOに新規追加
+    const newTodos = [...incompleteTodos, inputTodoName];
+    setIncompleteTodos(newTodos);
+    setInputTodoName("");
+  };
+  const onClickDelete = (deleteIndex) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(deleteIndex, 1); // deleteIndex番目から1つ削除する
+    setIncompleteTodos(newTodos);
+  };
+  const onClickComplete = (completeIndex) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    const newCompleteTodos = [
+      ...completeTodos,
+      newIncompleteTodos[completeIndex],
+    ];
+
+    newIncompleteTodos.splice(completeIndex, 1); // deleteIndex番目から1つ削除する
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+  const onClickBack = (backIndex) => {
+    const newCompleteTodos = [...completeTodos];
+    const newIncompleteTodos = [
+      ...incompleteTodos,
+      newCompleteTodos[backIndex],
+    ];
+
+    newCompleteTodos.splice(backIndex, 1); // deleteIndex番目から1つ削除する
+    setCompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newIncompleteTodos);
+  };
 
   return (
     <>
-      <ColorfulMessage color="Red">hello</ColorfulMessage>
-      <ColorfulMessage color="blue">hello2</ColorfulMessage>
-      <button onClick={onClickButton}>カウントアップ</button>
-      <p>{num}</p>
-
-      <button onClick={onClickSwitchShowFlag}>on/off</button>
-      {faceShowFlag && <p>face </p>}
+      <div class="input-area">
+        <input
+          placeholder="TODOを入力"
+          value={inputTodoName}
+          onChange={onTextChange}
+        />
+        <button onClick={onClickAdd}>追加</button>
+      </div>
+      <div class="incomplete-area">
+        <p class="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todoName, index) => {
+            return (
+              <li>
+                <div key={todoName} class="list-row">
+                  <p class="list-content">{todoName}</p>
+                  <button
+                    onClick={() => {
+                      onClickComplete(index);
+                    }}
+                  >
+                    完了
+                  </button>
+                  <button
+                    onClick={() => {
+                      onClickDelete(index);
+                    }}
+                  >
+                    削除
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div class="complete-area">
+        <p class="title">完了済みのTODO</p>
+        <ul>
+          {completeTodos.map((todoName, index) => {
+            return (
+              <li>
+                <div class="list-row">
+                  <p class="list-content">{todoName}</p>
+                  <button onClick={() => onClickBack(index)}>戻す</button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
